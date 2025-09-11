78e9274dfa59fc4e66ed3ccc670656eca997dd10
Sanjar-21 <oshiqdev1101@gmail.com>
2025-09-11 11:52:06 +0500

    cat class

---
+++ cat.py
@@ -0,0 +1,14 @@
+import sys
+# import os
+class Cat:
+    def __init__(self):
+       pass
+
+    def cat(self, file):
+        with open(file, "r") as files:
+            for file in files:
+                print(file, end="")
+
+if __name__ == '__main__':
+    cat = Cat()
+    cat.cat(sys.argv[1])
