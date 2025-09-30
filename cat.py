c0ef0f0316be6c47098a163d9a22bc279a77cfcd
Sanjar-21 <oshiqdev1101@gmail.com>
2025-09-30 09:35:31 +0500

    format file code

--- cat.py
+++ cat.py
@@ -1,15 +1,18 @@
 import sys
-# import os
+
+
 class Cat:
-    """ Linux cat command """
+    """Linux cat command"""
+
     def __init__(self):
-       pass
+        pass
 
     def cat(self, file):
         with open(file, "r") as files:
             for file in files:
                 print(file, end="")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     cat = Cat()
     cat.cat(sys.argv[1])
